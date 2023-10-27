b8e1cc910936b5f238a045ed1880f09b20a5975f
ChamiLamelas <chami.lamelas@gmail.com>
2023-10-27 00:03:11 -0400

    ran an epoch on windows

--- src/training.py
+++ src/training.py
@@ -10,6 +10,7 @@ import torch
 import prediction
 import device
 from torchvision.models.inception import InceptionOutputs
+from tqdm import tqdm
 
 
 def set_seed(seed):
@@ -22,10 +23,21 @@ def set_seed(seed):
     random.seed(seed)
 
 
-def train(seed, model, train_loader, test_loader, total_epochs, scale_up_epochs, scale_down_epochs, folder, init_optimizer, **optim_args):
+def train(
+    seed,
+    model,
+    train_loader,
+    test_loader,
+    total_epochs,
+    scale_up_epochs,
+    scale_down_epochs,
+    folder,
+    init_optimizer,
+    **optim_args,
+):
     set_seed(seed)
     logger = ML_Logger(log_folder=folder, persist=False)
-    logger.start(task='training', log_file='training', metrics_file='training')
+    logger.start(task="training", log_file="training", metrics_file="training")
     model = model.to(device.get_device())
     optimizer = init_optimizer(model.parameters(), **optim_args)
     scale_up_epochs = sorted(set(scale_up_epochs))
@@ -34,21 +46,29 @@ def train(seed, model, train_loader, test_loader, total_epochs, scale_up_epochs,
         if epoch in scale_up_epochs:
             config = scale_up_epochs[epoch]
             model = config["modifier"](
-                model, config["modify_args"]["ignore"], config["modify_args"]["modifier"])
+                model,
+                config["modify_args"]["ignore"],
+                config["modify_args"]["modifier"],
+            )
         elif epoch in scale_down_epochs:
             config = scale_down_epochs[epoch]
             model = config["modifier"](
-                model, config["modify_args"]["ignore"], config["modify_args"]["modifier"])
+                model,
+                config["modify_args"]["ignore"],
+                config["modify_args"]["modifier"],
+            )
         train_epoch(model, train_loader, epoch, optimizer, logger)
-        prediction.predict(model, train_loader, epoch, logger, 'train')
-        prediction.predict(model, test_loader, epoch, logger, 'test')
+        prediction.predict(model, train_loader, epoch, logger, "train")
+        prediction.predict(model, test_loader, epoch, logger, "test")
     logger.stop()
 
 
 def train_epoch(model, train_loader, epoch, optimizer, logger):
     model.train()
     total_loss = 0
-    for (data, target) in train_loader:
+    for data, target in tqdm(
+        train_loader, total=len(train_loader), desc=f"Epoch {epoch}"
+    ):
         data, target = device.move(device.get_device(), data, target)
         optimizer.zero_grad()
         output = model(data)
@@ -58,4 +78,4 @@ def train_epoch(model, train_loader, epoch, optimizer, logger):
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
-    logger.log_metrics({'epoch': epoch, 'train_loss': total_loss})
+    logger.log_metrics({"epoch": epoch, "train_loss": total_loss})
