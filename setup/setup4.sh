3f9ee65572335bbe490f9167c47670cdb6a27b20
ChamiLamelas <chami.lamelas@gmail.com>
2023-10-27 15:41:26 -0500

    add git setup to setup

--- setup/setup4.sh
+++ setup/setup4.sh
@@ -4,3 +4,5 @@ sudo apt install python3-pip
 python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 python3 -m pip install toml
 
+git config --global user.name ChamiLamelas
+git config --global user.email chami.lamelas@gmail.com
