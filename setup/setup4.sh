a836a503eecac049d5ce46a108052c10ab9ab6d2
ChamiLamelas <chami.lamelas@gmail.com>
2023-10-28 16:13:32 -0500

    add -y to installs

--- setup/setup4.sh
+++ setup/setup4.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-sudo apt install python3-pip
-python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-python3 -m pip install toml
+sudo apt install -y python3-pip
+python3 -m pip install -y torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+python3 -m pip install -y toml
 
 git config --global user.name ChamiLamelas
 git config --global user.email chami.lamelas@gmail.com
