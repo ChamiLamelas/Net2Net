7a0fb53989abaf7bba11980508ff916ccbabb432
ChamiLamelas <chami.lamelas@gmail.com>
2023-11-07 08:11:05 -0600

    fix setup4

--- setup/setup4.sh
+++ setup/setup4.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo THIS SCRIPT REQUIRES USER INPUT -- DONT LEAVE
+# note this script hangs at various points (e.g. at eta 0:00:01 for torch, after matploatlib, etc.)
 
 # 1st part taken from here: https://linuxize.com/post/how-to-install-python-3-8-on-ubuntu-18-04/
 # the default python version is 3.6
@@ -12,6 +13,9 @@ sudo apt install python3.8
 
 # this I figured out was necessary because otherwise torch would appear to install then show up
 # nowhere in the system whether you run pip show/list on any of the installed pip versions
+# I also figured out that the second step requires some pip installation to exist - so that
+# is done normally first 
+sudo apt-get install python3-pip
 python3.8 -m pip install -U pip
 
 # this first step is taken from here: https://pytorch.org/get-started/locally/
