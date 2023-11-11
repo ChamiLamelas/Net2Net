028c00d3676aff9a7a9511a26df1537594516cb8
ChamiLamelas <chami.lamelas@gmail.com>
2023-11-11 12:13:46 -0600

    add imagenet test remove

--- setup/kaggleprep.sh
+++ setup/kaggleprep.sh
@@ -6,10 +6,11 @@ kaggle competitions download -c imagenet-object-localization-challenge
 mkdir ImageNet
 time unzip imagenet-object-localization-challenge.zip -d ImageNet >& /dev/null
 mv ImageNet Net2Net/data
-rm imagenet-object-localization-challenge.zip
+rm -rf imagenet-object-localization-challenge.zip ~/Net2Net/data/ImageNet/ILSVRC/Data/CLS-LOC/test
 
 # Taken from here: https://github.com/facebookresearch/kill-the-bits/issues/5,
 # https://discuss.pytorch.org/t/issues-with-dataloader-for-imagenet-should-i-use-datasets-imagefolder-or-datasets-imagenet/115742/7
 # Related error discussed here: 
 # https://stackoverflow.com/questions/69199273/torchvision-imagefolder-could-not-find-any-class-folder
 time bash valprep.sh
+
